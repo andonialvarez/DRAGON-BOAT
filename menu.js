7e0c1576380bf85cb5c5434feca7817b2193c06b
andonialvarez <andonialvarez3@gmail.com>
2024-04-05 21:29:04 +0200

    mas media query

--- menu.js
+++ menu.js
@@ -1,14 +1,17 @@
 const openMenu = document.querySelector(".open-menu");
 const closeMenu = document.querySelector("#close-menu");
 const aside = document.querySelector(".aside");
+const volver = document.querySelector(".volver")
 
 openMenu.addEventListener("click", () => {
     aside.classList.add("aside-visible");
     aside.classList.remove("aside");
+    volver.classList.remove("volver")
     console.log("open")
 })
 
 closeMenu.addEventListener("click", () => {
     aside.classList.remove("aside-visible");
     aside.classList.add("aside");
+    cargarProductos(productos);
 })
